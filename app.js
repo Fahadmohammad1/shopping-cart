d406098d6d76c0f917ebb700b6a1aab0e983aaa6
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-02-13 22:00:03 +0600

    complete

--- app.js
+++ app.js
@@ -1,26 +1,57 @@
+// phone
+function updatePhoneNumber(product, price, isAdding) {
+  const input = document.getElementById(product + "-input");
+  const quantity = input.value;
+  if (isAdding == true) {
+    input.value = parseInt(quantity) + 1;
+  } else if (isAdding == false && quantity > 0) {
+    input.value = parseInt(quantity) - 1;
+  }
+  const phoneFinal = input.value;
+
+  // price update
+  const phonePrice = document.getElementById(product + "-price");
+  const phoneTotal = phoneFinal * price;
+  phonePrice.innerText = phoneTotal;
+
+  // calcultate total
+  calculateTotal();
+}
+function getInputValue(products) {
+  const input = document.getElementById(products + "-input");
+  const productsQuantity = parseInt(input.value);
+  return productsQuantity;
+}
+function calculateTotal() {
+  const phoneTotal = getInputValue("phone") * 1219;
+  const caseTotal = getInputValue("case") * 59;
+  const subtotal = phoneTotal + caseTotal;
+  const tax = subtotal / 10;
+  const total = subtotal + tax;
+
+  document.getElementById("subtotal-amount").innerText = subtotal;
+  document.getElementById("total-tax").innerText = tax;
+  document.getElementById("total-amount").innerText = total;
+}
+
+// phone plus
+document.getElementById("phone-plus").addEventListener("click", function () {
+  updatePhoneNumber("phone", 1219, true);
+});
+// phone minus
+document.getElementById("phone-minus").addEventListener("click", function () {
+  updatePhoneNumber("phone", 1219, false);
+});
+
 // case
 document
   .getElementById("case-increase-btn")
   .addEventListener("click", function () {
-    const casingInput = document.getElementById("case-input");
-    const caseQuantity = casingInput.value;
-    casingInput.value = parseInt(caseQuantity) + 1;
-    const casingFinal = casingInput.value;
-    // price update
-    const plusPrice = document.getElementById("case-price");
-    plusTotal = casingFinal * 59;
-    plusPrice.innerText = plusTotal;
+    updatePhoneNumber("case", 59, true);
   });
 
 document
   .getElementById("case-decrease-btn")
   .addEventListener("click", function () {
-    const minusInput = document.getElementById("case-input");
-    const minusQuantity = minusInput.value;
-    minusInput.value = parseInt(minusQuantity) - 1;
-    const minusFinal = minusInput.value;
-    // minus price update
-    const minusPrice = document.getElementById("case-price");
-    minustotal = minusFinal * 59;
-    minusPrice.innerText = minustotal;
+    updatePhoneNumber("case", 59, false);
   });
